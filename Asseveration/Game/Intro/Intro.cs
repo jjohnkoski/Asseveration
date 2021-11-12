9b3121de8bb61abb61d8f7141e3447087318ebef
Jacob Johnkoski <jacobjohnkoski@gmail.com>
2021-11-11 18:56:44 -0600

    Intro scene now uses child instance of MusicPlayer to play correct intro music

--- Asseveration/Game/Intro/Intro.cs
+++ Asseveration/Game/Intro/Intro.cs
@@ -3,8 +3,15 @@ using System;
 
 public class Intro : Node2D
 {
+    [Signal]
+    public delegate void PlaySelectedTrack(string fileName);
     public override void _Ready()
     {
+        PlayIntroMusic();
+    }
 
+    public void PlayIntroMusic()
+    {
+        EmitSignal("PlaySelectedTrack", "The End is the Beginning is the End.wav");
     }
 }
